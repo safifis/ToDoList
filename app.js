6e1b36f81d454d85bb1e977ab233f7061c08592a
Feifei Sun <feifeisun0503@gmail.com>
2023-05-20 13:36:58 -0400

    Add date module and refactor code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const https = require("https");
+const date = require(__dirname + "/date.js")
 
 const app = express();
 app.use(express.urlencoded({extended: true}));
@@ -7,24 +8,18 @@ app.use(express.static("public"));
 
 app.set("view engine", "ejs")
 
-let items = ["Buy grocery", "Prepare food", "Do Laundry"];
-let workItems = [];
+const items = ["Buy grocery", "Prepare food", "Do Laundry"];
+const workItems = [];
 
 app.get("/", (req, res) => {
-  let today = new Date();
 
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {listTitle: day, newListItems: items, route: "/"});
 });
 
 app.post("/", (req, res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
     items.push(item);
     res.redirect("/");
 });
@@ -34,7 +29,7 @@ app.get("/work", (req, res) => {
 });
 
 app.post("/work", (req, res) => {
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
